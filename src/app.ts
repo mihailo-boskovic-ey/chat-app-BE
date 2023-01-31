ea09f741762ea21e72ba1875eef3762cbb231ec6
Mihailo Boskovic <mihailo.boskovic@rs.ey.com>
2023-01-31 13:56:03 +0100

    setup expressJS

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,27 @@
+import express from 'express';
 import * as dotenv from 'dotenv';
+import { Server } from 'socket.io';
+import { Request, Response } from 'express';
+import helmet from 'helmet';
 dotenv.config();
+
+const app = express();
+const server = require('http').createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.URL,
+    credentials: true,
+  },
+});
+
+app.use(helmet());
+app.use(express.json());
+
+app.get('/', (_req: Request, res: Response): void => {
+  res.send('hi');
+});
+
+io.on('connect', (socket) => {});
+server.listen(4000, (): void => {
+  console.log('Server listening on port 4000');
+});
